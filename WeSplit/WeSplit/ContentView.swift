b865f9865033ab80040e3457c31e97ef72de4db1
Thomas Kellough <thomas.kellough@brandtinfo.com>
2020-07-07 15:42:04 -0500

    added navigation title

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -10,30 +10,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Form {
-            // Limit of 10! After 10, use groups
-            // You can also break it apart into sections
-            Section {
-                Text("Hello, World!")
-                Text("Hello, Old World!")
-                Text("Hello, New World!")
-                Text("Hello, World!")
-                Text("Hello, Old World!")
+        // Add a navigation bar
+        NavigationView {
+            Form {
+                // Limit of 10! After 10, use groups
+                // You can also break it apart into sections
+                Section {
+                    Text("Hello, World!")
+                    Text("Hello, Old World!")
+                    Text("Hello, New World!")
+                    Text("Hello, World!")
+                    Text("Hello, Old World!")
+                }
+                Group {
+                    Text("Hello, World!")
+                    Text("Hello, Old World!")
+                    Text("Hello, New World!")
+                    Text("Hello, World!")
+                    Text("Hello, Old World!")
+                }
+                Section {
+                    Text("Hello, World!")
+                    Text("Hello, Old World!")
+                    Text("Hello, New World!")
+                    Text("Hello, World!")
+                    Text("Hello, Old World!")
+                }
             }
-            Group {
-                Text("Hello, World!")
-                Text("Hello, Old World!")
-                Text("Hello, New World!")
-                Text("Hello, World!")
-                Text("Hello, Old World!")
-            }
-            Section {
-                Text("Hello, World!")
-                Text("Hello, Old World!")
-                Text("Hello, New World!")
-                Text("Hello, World!")
-                Text("Hello, Old World!")
-            }
+//             Use modifiers to edit specific components
+//            .navigationBarTitle(Text("SwiftUI"))
+//             You can also use small titles by default
+//                .navigationBarTitle(Text("SwiftUI"), displayMode: .inline)
+//            You can also just use a shortcut for both since it's so common
+            .navigationBarTitle("SwiftUI")
         }
     }
 }
