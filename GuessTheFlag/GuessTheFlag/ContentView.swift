e1e0bdcedea43a200bf3d31944ce892912a19b6a
Thomas Kellough <thomas.kellough@brandtinfo.com>
2020-07-08 11:27:07 -0500

    Challenge 3 completed

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -65,7 +65,7 @@ struct ContentView: View {
             scoreTitle = "Correct"
             score += 1
         } else {
-            scoreTitle = "Wrong"
+            scoreTitle = "Wrong! That's the flag of \(countries[number])"
         }
         
         showingScore = true
