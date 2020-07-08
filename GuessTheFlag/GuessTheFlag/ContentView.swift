dcc3063b8b1752f51ef8bf70b2977467585ce9f3
Thomas Kellough <thomas.kellough@brandtinfo.com>
2020-07-07 22:24:16 -0500

    added flags  as buttons

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -10,14 +10,32 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var showingAlert = false
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    var correctAnwser = Int.random(in: 0...2)
     
     var body: some View {
-        Button("Show Alert") {
-            self.showingAlert = true
-        }
-        .alert(isPresented: $showingAlert) {
-            Alert(title: Text("This is a title"), message: Text("This is the message"), dismissButton: .default(Text("Okay")))
+        ZStack {
+            Color.blue.edgesIgnoringSafeArea(.all)
+            
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundColor(.white)
+                    Text(countries[correctAnwser])
+                        .foregroundColor(.white)
+                }
+                
+                ForEach(0 ..< 3) { number in
+                    Button(action: {
+                        // flag that was tapped
+                    }) {
+                        Image(self.countries[number])
+                            .renderingMode(.original)
+                    }
+                }
+                Spacer()
+            }
+            
         }
     }
 }
