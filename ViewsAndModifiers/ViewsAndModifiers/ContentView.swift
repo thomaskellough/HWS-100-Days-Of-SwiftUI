095282a7471cbfcec0be64314cacf17cebdecd95
Thomas Kellough <thomas.kellough@brandtinfo.com>
2020-07-12 12:28:07 -0600

    added custom modifiers

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -16,6 +16,14 @@ struct ContentView: View {
                 .foregroundColor(Color.yellow)
             CapsuleText(text: "Second")
                 .foregroundColor(Color.pink)
+            Text("Hello, Title")
+                .modifier(Title())
+            Text("Hello, Subtitle")
+                .subtitleStyle()
+            Text("Watermarked!")
+                .frame(width: 300, height: 200)
+                .background(Color.yellow)
+                .watermarked(with: "Hacking With Swift")
         }
     }
 }
@@ -26,14 +34,66 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
+// Custom View
 struct CapsuleText: View {
     var text: String
-
+    
     var body: some View {
         Text(text)
             .font(.largeTitle)
             .padding()
             .background(Color.blue)
             .clipShape(Capsule())
+    }
+}
+
+// Custom View Modifier
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(Color.white)
+            .padding()
+            .background(Color.red)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+
+struct Subtitle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.subheadline)
+            .foregroundColor(Color.white)
+            .padding()
+            .background(Color.green)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+
+// It can be a better idea to create an extension on View to make modifiers easier to use
+extension View {
+    func subtitleStyle() -> some View {
+        self.modifier(Subtitle())
+    }
+}
+
+struct Watermark: ViewModifier {
+    var text: String
+    
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.white)
+                .padding()
+                .background(Color.black)
+        }
+    }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        self.modifier(Watermark(text: text))
     }
 }
