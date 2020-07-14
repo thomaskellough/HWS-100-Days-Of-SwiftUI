eca50a6bda4553717047c85ce0d0932c092449ea
Thomas Kellough <thomas.r.kellough@gmail.com>
2020-07-13 21:26:23 -0600

    added dates

--- BetterRest/BetterRest/PracticeContentView.swift
+++ BetterRest/BetterRest/PracticeContentView.swift
@@ -11,14 +11,29 @@ import SwiftUI
 struct PracticeContentView: View {
     
     @State private var sleepAmount = 8.0
+    @State private var wakeUp = Date()
+    let now = Date()
+    let tomorrow = Date().addingTimeInterval(86400)
     
     var body: some View {
+        
         VStack {
             Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
                 // %.2g removes trailing insignificant 0's
                 Text("\(sleepAmount, specifier: "%.2g") hours")
             }
+            Form {
+                DatePicker("Please select a date", selection: $wakeUp, displayedComponents: .date)
+                DatePicker("Please select a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+            }
+            // This still allows for voice over, but hides the label
+            DatePicker("Please enter a date", selection: $wakeUp)
+                .labelsHidden()
+            
+            DatePicker("Please enter date", selection: $wakeUp, in: Date()...tomorrow)
+            DatePicker("Please enter date", selection: $wakeUp, in: Date()...)
         }
+        
     }
 }
 
