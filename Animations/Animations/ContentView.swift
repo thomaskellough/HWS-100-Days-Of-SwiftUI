a3d7361853646b2d2f506343497504f1cf3425c4
Thomas Kellough <thomas.r.kellough@gmail.com>
2020-08-25 21:45:46 -0500

    controlling the animation stack

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var animationAmount = 0.0
+    @State private var enabled = false
     
     var body: some View {
         VStack {
@@ -22,6 +23,7 @@ struct ContentView: View {
             .styleButton(color: Color.red)
             .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
             .padding()
+            
             Button("Flip X") {
                 withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
                     self.animationAmount += 360
@@ -30,6 +32,7 @@ struct ContentView: View {
             .styleButton(color: Color.blue)
             .rotation3DEffect(.degrees(animationAmount), axis: (x: 1, y: 0, z: 0))
             .padding()
+            
             Button("Flip Z") {
                 withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
                     self.animationAmount += 360
@@ -37,6 +40,17 @@ struct ContentView: View {
             }
             .styleButton(color: Color.green)
             .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 0, z: 1))
+            .padding()
+            
+            Button("Color Change") {
+                self.enabled.toggle()
+            }
+            .frame(width: 200, height: 200)
+            .styleButtonStack(color: enabled ? Color.yellow : Color.purple)
+            .animation(.default)
+            .foregroundColor(.black)
+            .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
+            .animation(.interpolatingSpring(stiffness: 10, damping: 1))
         }
     }
 }
@@ -47,8 +61,7 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-// Custom View Modifier
-
+// Custom View Modifier for Explicit Animations
 struct ButtonStyle: ViewModifier {
     var color: Color
     
@@ -64,5 +77,23 @@ struct ButtonStyle: ViewModifier {
 extension View {
     func styleButton(color: Color) -> some View {
         self.modifier(ButtonStyle(color: color))
+    }
+}
+
+// CustomView Modifer for Animation Stack
+struct ButtonStyleStack: ViewModifier {
+    var color: Color
+    
+    func body(content: Content) -> some View {
+        content
+            .padding(50)
+            .background(color)
+            .foregroundColor(.white)
+    }
+}
+
+extension View {
+    func styleButtonStack(color: Color) -> some View {
+        self.modifier(ButtonStyleStack(color: color))
     }
 }
