e69687e6448695f2657867f906b7e32df328d6ac
Thomas Kellough <thomas.r.kellough@gmail.com>
2020-08-26 22:25:21 -0500

    added custom transitions using ViewModifier

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -15,45 +15,61 @@ struct ContentView: View {
     
     @State private var dragAmount = CGSize.zero
     
+    @State private var isShowingRed = false
+    
     let letters = Array("Hello SwiftUI")
     
     var body: some View {
         
         VStack {
-            
-            HStack(spacing: 0) {
-                ForEach(0..<letters.count) { num in
-                    Text(String(self.letters[num]))
-                        .padding(5)
-                        .font(.title)
-                        .background(self.enabled ? Color.blue : Color.red)
-                        .offset(self.dragAmount)
-                        .animation(Animation.default.delay(Double(num) / 20))
+            Button("Tap Me") {
+                withAnimation {
+                    self.isShowingRed.toggle()
                 }
             }
-            .gesture(
-                DragGesture()
-                    .onChanged { self.dragAmount = $0.translation }
-                    .onEnded { _ in
-                        self.dragAmount = .zero
-                        self.enabled.toggle()
-                }
-            )
             
-            LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
-                .frame(width: 300, height: 200)
-                .clipShape(RoundedRectangle(cornerRadius: 10))
-                .offset(dragAmount)
-                .gesture(
-                    DragGesture()
-                        .onChanged { self.dragAmount = $0.translation }
-                        .onEnded {  _ in
-                            withAnimation(.spring()) {
-                                self.dragAmount = .zero
-                            }
-                        }
-                )
-                .animation(.spring())
+            if isShowingRed {
+                Rectangle()
+                    .fill(Color.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.pivot)
+//                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+//                    .transition(.pivot)
+            }
+            
+//            HStack(spacing: 0) {
+//                ForEach(0..<letters.count) { num in
+//                    Text(String(self.letters[num]))
+//                        .padding(5)
+//                        .font(.title)
+//                        .background(self.enabled ? Color.blue : Color.red)
+//                        .offset(self.dragAmount)
+//                        .animation(Animation.default.delay(Double(num) / 20))
+//                }
+//            }
+//            .gesture(
+//                DragGesture()
+//                    .onChanged { self.dragAmount = $0.translation }
+//                    .onEnded { _ in
+//                        self.dragAmount = .zero
+//                        self.enabled.toggle()
+//                }
+//            )
+//
+//            LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
+//                .frame(width: 300, height: 200)
+//                .clipShape(RoundedRectangle(cornerRadius: 10))
+//                .offset(dragAmount)
+//                .gesture(
+//                    DragGesture()
+//                        .onChanged { self.dragAmount = $0.translation }
+//                        .onEnded {  _ in
+//                            withAnimation(.spring()) {
+//                                self.dragAmount = .zero
+//                            }
+//                        }
+//                )
+//                .animation(.spring())
             
             
             
@@ -138,5 +154,23 @@ struct ButtonStyleStack: ViewModifier {
 extension View {
     func styleButtonStack(color: Color) -> some View {
         self.modifier(ButtonStyleStack(color: color))
+    }
+}
+
+struct CornerRotateModifer: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body(content: Content) -> some View {
+        content.rotationEffect(.degrees(amount), anchor: anchor).clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifer(amount: -90, anchor: .topLeading),
+            identity: CornerRotateModifer(amount: 0, anchor: .topLeading)
+        )
     }
 }
