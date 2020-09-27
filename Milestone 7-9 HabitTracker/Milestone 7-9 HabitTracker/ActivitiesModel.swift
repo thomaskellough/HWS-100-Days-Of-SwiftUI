713afb3e9849a2f05aba60979edcdcda18f52d3d
Thomas Kellough <thomas.r.kellough@gmail.com>
2020-09-27 12:04:17 -0500

    completed milestone 7-9 habit tracker

--- Milestone 7-9 HabitTracker/Milestone 7-9 HabitTracker/ActivitiesModel.swift
+++ Milestone 7-9 HabitTracker/Milestone 7-9 HabitTracker/ActivitiesModel.swift
@@ -8,11 +8,30 @@
 import Foundation
 
 class Activities: ObservableObject {
-    @Published var activities = [Activity]()
+    @Published var activities: [Activity] {
+        didSet {
+            let encoder = JSONEncoder()
+            if let encoded = try? encoder.encode(activities) {
+                UserDefaults.standard.setValue(encoded, forKey: "activities")
+            }
+        }
+    }
+    
+    init() {
+        if let activities = UserDefaults.standard.data(forKey: "activities") {
+            let decoder = JSONDecoder()
+            if let decoded = try? decoder.decode([Activity].self, from: activities) {
+                self.activities = decoded
+                return
+            }
+        }
+        
+        self.activities = []
+    }
 }
 
-struct Activity: Identifiable {
-    let id = UUID()
+struct Activity: Codable, Identifiable {
+    var id = UUID()
     let name: String
     let description: String
     var count: Int
