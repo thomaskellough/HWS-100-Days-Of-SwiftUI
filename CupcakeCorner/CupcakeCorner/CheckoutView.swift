51270a6db23740653f8a6dcb5d9fdb1bcd602b2b
Thomas Kellough <thomas.kellough@brandtinfo.com>
2020-10-17 20:09:51 -0600

    completed challenge 2

--- CupcakeCorner/CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CupcakeCorner/CheckoutView.swift
@@ -10,8 +10,9 @@ import SwiftUI
 struct CheckoutView: View {
     @ObservedObject var order: Order
     
-    @State private var confirmationMessage = ""
-    @State private var showingConfirmation = false
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
     
     var body: some View {
         GeometryReader { geo in
@@ -33,8 +34,8 @@ struct CheckoutView: View {
             }
         }
         .navigationBarTitle("Check Out", displayMode: .inline)
-        .alert(isPresented: $showingConfirmation) {
-            Alert(title: Text("Thank you!"), message: Text(confirmationMessage), dismissButton: .default(Text("Okay")))
+        .alert(isPresented: $showingAlert) {
+            Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("Okay")))
         }
     }
     
@@ -53,13 +54,16 @@ struct CheckoutView: View {
         
         URLSession.shared.dataTask(with: request) { data, response, error in
             guard let data = data else {
-                print("No data in response. \(error?.localizedDescription ?? "Unknown error")")
+                self.alertTitle = "Error"
+                self.alertMessage = "There was an error sending data to the server. Please check your internet connection and try again."
+                self.showingAlert = true
                 return
             }
             
             if let decoded = try? JSONDecoder().decode(Order.self, from: data) {
-                self.confirmationMessage = "Your order for \(decoded.quantity)x \(Order.types[decoded.type].lowercased()) cupcakes is on its way!"
-                self.showingConfirmation = true
+                self.alertTitle = "Thank you!"
+                self.alertMessage = "Your order for \(decoded.quantity)x \(Order.types[decoded.type].lowercased()) cupcakes is on its way!"
+                self.showingAlert = true
             } else {
                 print("Invalid response from server")
             }
