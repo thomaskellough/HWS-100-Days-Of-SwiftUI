12c2054751b6e77a15148cf9aa9d74eaec18e0be
thomas.kellough <thomas.kellough@brandtinfo.com>
2020-11-21 14:42:18 -0700

    core data tutorial init commit

---
+++ CoreDataProject/CoreDataProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreDataProject
+//
+//  Created by Thomas Kellough on 11/21/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
