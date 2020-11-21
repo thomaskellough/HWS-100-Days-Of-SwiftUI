83c285c734686582650bcfe5ac050f2b38bd1793
thomas.kellough <thomas.kellough@brandtinfo.com>
2020-11-21 14:57:45 -0700

    handling of unknown title values

---
+++ CoreDataProject/CoreDataProject/Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Thomas Kellough on 11/21/20.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
