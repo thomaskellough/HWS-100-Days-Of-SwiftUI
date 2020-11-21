a945f2166547079a8dc8b3ec593b89e30b248f27
thomas.kellough <thomas.kellough@brandtinfo.com>
2020-11-20 18:17:35 -0700

    completed challenge 2

--- Bookwork/Bookwork/Views/ContentView.swift
+++ Bookwork/Bookwork/Views/ContentView.swift
@@ -29,6 +29,7 @@ struct ContentView: View {
                         VStack(alignment: .leading) {
                             Text(book.title ?? "Unkown title")
                                 .font(.headline)
+                                .foregroundColor(book.rating == 1 ? .red : .primary)
                             Text(book.author ?? "Unknown author")
                                 .foregroundColor(.secondary)
                         }
