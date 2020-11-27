3fc344be1a61538375774211da5bc8b4c80e0813
thomas.kellough <thomas.r.kellough@gmail.com>
2020-11-26 18:13:27 -0700

    wrapping UIViewController in SwiftUI view

---
+++ Instafilter/Instafilter/ImagePicker.swift
@@ -0,0 +1,20 @@
+//
+//  ImagePicker.swift
+//  Instafilter
+//
+//  Created by Thomas Kellough on 11/26/20.
+//
+
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+        
+    }
+    
+}
