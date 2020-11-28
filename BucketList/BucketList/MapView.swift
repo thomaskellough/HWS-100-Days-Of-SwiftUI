88daec7ea44f18f0bde38b77e0bfc1569da4e0b5
thomas.kellough <thomas.r.kellough@gmail.com>
2020-11-27 18:34:11 -0700

    completed communications with a market coordinator

--- BucketList/BucketList/MapView.swift
+++ BucketList/BucketList/MapView.swift
@@ -11,11 +11,50 @@ import SwiftUI
 struct MapView: UIViewRepresentable {
     func makeUIView(context: UIViewRepresentableContext<MapView>) -> some MKMapView {
         let mapView = MKMapView()
+        mapView.delegate = context.coordinator
+        
+        let annotation = MKPointAnnotation()
+        annotation.title = "London"
+        annotation.subtitle = "Capital of England"
+        annotation.coordinate = CLLocationCoordinate2D(latitude: 51.5, longitude: 0.13)
+        mapView.addAnnotation(annotation)
+        
         return mapView
     }
     
     func updateUIView(_ uiView: UIViewType, context: Context) {
     }
+}
+
+// MARK: Coordinator Functionality
+extension MapView {
+    class Coordinator: NSObject, MKMapViewDelegate {
+        var parent: MapView
+        
+        // Notifies when map view changes location
+        func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+            print(mapView.centerCoordinate)
+        }
+        
+        // Customize how the annotations look
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            let view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: nil)
+            view.canShowCallout = true
+            view.pinTintColor = .purple
+            view.image = UIImage(systemName: "bolt")
+            
+            return view
+        }
+        
+        init(_ parent: MapView) {
+            self.parent = parent
+        }
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
 }
 
 struct MapView_Previews: PreviewProvider {
