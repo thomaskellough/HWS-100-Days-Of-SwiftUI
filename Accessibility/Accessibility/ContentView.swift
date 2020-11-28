62e5b66539130bae72ab5d03d356169b8967c5a8
thomas.kellough <thomas.r.kellough@gmail.com>
2020-11-28 16:03:08 -0700

    accessibility for slider values - default to percentage, can override

--- Accessibility/Accessibility/ContentView.swift
+++ Accessibility/Accessibility/ContentView.swift
@@ -8,30 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
-    let pictures = [
-        "ales-krivec-15949",
-        "galina-n-189483",
-        "kevin-horstmann-141705",
-        "nicolas-tissot-335096"
-    ]
-    
-    let labels = [
-        "Tulips",
-        "Frozen tree buds",
-        "Sunflowers",
-        "Fireworks"
-    ]
-    
-    @State private var selectedPicture = Int.random(in: 0...3)
+    @State private var estimate = 25.0
     
     var body: some View {
         VStack {
-            Text("Your score is")
-            Text("1000")
-                .font(.title)
+            Slider(value: $estimate, in: 0...50)
+                .padding()
+                .accessibility(value: Text("\(Int(estimate))"))
         }
-        .accessibilityElement(children: .ignore)
-        .accessibility(label: Text("Your score is 1000"))
     }
 
 }
