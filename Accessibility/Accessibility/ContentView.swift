486086944e56e90a0699f4c2144962d23fa57e36
thomas.kellough <thomas.r.kellough@gmail.com>
2020-11-28 16:08:49 -0700

    added accessibility for stepper - currently it doesn't read out by default

--- Accessibility/Accessibility/ContentView.swift
+++ Accessibility/Accessibility/ContentView.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var estimate = 25.0
+    @State private var rating = 3
     
     var body: some View {
         VStack {
+            Stepper("Rate our service: \(rating)/5", value: $rating, in: 1...5)
+                .accessibility(value: Text("\(rating) out of 5"))
             Slider(value: $estimate, in: 0...50)
                 .padding()
                 .accessibility(value: Text("\(Int(estimate))"))
