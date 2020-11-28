3d0b0d525f4bb1da17f5b5bffff4ac11a02a6059
thomas.kellough <thomas.r.kellough@gmail.com>
2020-11-28 15:16:29 -0700

    init commit accessibility - added label and trait modifiers

---
+++ Accessibility/Accessibility/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  Accessibility
+//
+//  Created by Thomas Kellough on 11/28/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let pictures = [
+        "ales-krivec-15949",
+        "galina-n-189483",
+        "kevin-horstmann-141705",
+        "nicolas-tissot-335096"
+    ]
+    
+    let labels = [
+        "Tulips",
+        "Frozen tree buds",
+        "Sunflowers",
+        "Fireworks"
+    ]
+    
+    @State private var selectedPicture = Int.random(in: 0...3)
+    
+    var body: some View {
+        Image(pictures[selectedPicture])
+            .resizable()
+            .scaledToFit()
+            .onTapGesture {
+                self.selectedPicture = Int.random(in: 0...3)
+            }
+            .accessibility(label: Text(labels[selectedPicture]))
+            .accessibility(addTraits: .isButton)
+            .accessibility(removeTraits: .isImage)
+    }
+
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
