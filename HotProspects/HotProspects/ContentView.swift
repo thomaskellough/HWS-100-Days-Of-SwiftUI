fd5f7b1d60c3096f2f2523f386ab61e46b908910
thomas.kellough <thomas.r.kellough@gmail.com>
2020-11-29 08:03:40 -0700

    init commit HotProspects - reading custom values from environment with @EnvironmentObject

---
+++ HotProspects/HotProspects/ContentView.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by Thomas Kellough on 11/29/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let user = User()
+    
+    var body: some View {
+        VStack {
+            EditView()
+            DisplayView()
+        }
+        .environmentObject(user)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+class User: ObservableObject {
+    @Published var name = "Taylor Swift"
+}
+
+struct EditView: View {
+    @EnvironmentObject var user: User
+    
+    var body: some View {
+        TextField("Name", text: $user.name)
+    }
+}
+
+struct DisplayView: View {
+    @EnvironmentObject var user: User
+    
+    var body: some View {
+        Text(user.name)
+    }
+}
