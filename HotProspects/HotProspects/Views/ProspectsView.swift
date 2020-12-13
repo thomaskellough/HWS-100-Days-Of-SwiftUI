89a6d902dd9ced7244dc9886a44340391e598685
thomas.kellough <thomas.r.kellough@gmail.com>
2020-12-12 23:15:16 -0700

    added ability to save data using user defaults

--- HotProspects/HotProspects/Views/ProspectsView.swift
+++ HotProspects/HotProspects/Views/ProspectsView.swift
@@ -50,6 +50,11 @@ struct ProspectsView: View {
                         Text(prospect.emailAddress)
                             .foregroundColor(.secondary)
                     }
+                    .contextMenu(ContextMenu(menuItems: {
+                        Button(prospect.isContacted ? "Mark Uncontacted" : "Mark Contacted") {
+                            self.prospects.toggle(prospect)
+                        }
+                    }))
                 }
             }
             .navigationBarTitle(title)
@@ -76,7 +81,7 @@ struct ProspectsView: View {
             person.name = details[0]
             person.emailAddress = details[1]
             
-            self.prospects.people.append(person)
+            self.prospects.add(person)
         case .failure(let error):
             print("Scanning failed: \(error.localizedDescription)")
         }
