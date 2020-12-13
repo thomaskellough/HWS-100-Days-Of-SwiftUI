6cd068319dc3134dc8694fec2bc2d97780796e96
thomas.kellough <thomas.r.kellough@gmail.com>
2020-12-12 20:26:48 -0700

    learned about image interpolation

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -7,66 +7,24 @@
 
 import SwiftUI
 
-enum NetworkError: Error {
-    case badURL, requestFailed, unknown
-}
-
 struct ContentView: View {
-    @ObservedObject var updater = DelayedUpdater()
 
     var body: some View {
         VStack {
-            Text("Value is: \(updater.value)")
-            Button("Tap me") {
-                buttonTapped()
-            }
+            Image("example")
+                .interpolation(.none)
+                .resizable()
+                .scaledToFit()
+                .frame(maxHeight: .infinity)
+                .background(Color.black)
+                .edgesIgnoringSafeArea(.all)
         }
-    }
-    
-    func buttonTapped() {
-        print(self.updater.value)
-    }
-    
-    func fetchData(from urlString: String, completion: @escaping (Result<String, NetworkError>) -> Void) {
-        guard let url = URL(string: urlString) else {
-            completion(.failure(.badURL))
-            return
-        }
-        
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            DispatchQueue.main.async {
-                if let data = data {
-                    let string = String(data: data, encoding: .utf8)
-                    completion(.success(string!))
-                } else if error != nil {
-                    completion(.failure(.requestFailed))
-                } else {
-                    completion(.failure(.unknown))
-                }
-            }
-        }.resume()
+
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-    }
-}
-
-
-class DelayedUpdater: ObservableObject {
-    var value = 0 {
-        willSet {
-            objectWillChange.send()
-        }
-    }
-    
-    init() {
-        for i in 1...10 {
-            DispatchQueue.main.asyncAfter(deadline: .now() + Double(i)) {
-                self.value += 1
-            }
-        }
     }
 }
