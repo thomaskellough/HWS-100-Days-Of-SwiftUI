1d07b5cdef6fd4bded3163413342e3d0e3ea6f8c
thomas.kellough <thomas.r.kellough@gmail.com>
2020-12-12 19:20:58 -0700

    demonstration of tabs

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -8,14 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
-    let user = User()
-    
+    @State private var selectedTab = "Tab 1 View"
+
     var body: some View {
-        VStack {
-            EditView()
-            DisplayView()
+        TabView(selection: $selectedTab) {
+            Text("Tab 1")
+                .onTapGesture {
+                    self.selectedTab = "Tab 2 View"
+                }
+                .tabItem {
+                    Image(systemName: "star")
+                    Text("Tab One")
+                }
+                .tag("Tab 1 View")
+            Text("Tab 2")
+                .onTapGesture {
+                    self.selectedTab = "Tab 1 View"
+                }
+                .tabItem {
+                    Image(systemName: "star.fill")
+                    Text("Tab Two")
+                }
+                .tag("Tab 2 View")
         }
-        .environmentObject(user)
     }
 }
 
@@ -25,22 +40,3 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-class User: ObservableObject {
-    @Published var name = "Taylor Swift"
-}
-
-struct EditView: View {
-    @EnvironmentObject var user: User
-    
-    var body: some View {
-        TextField("Name", text: $user.name)
-    }
-}
-
-struct DisplayView: View {
-    @EnvironmentObject var user: User
-    
-    var body: some View {
-        Text(user.name)
-    }
-}
