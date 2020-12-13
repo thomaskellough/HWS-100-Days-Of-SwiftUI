fd7b0d2c539ad1fcffc8df4303ffc13d804c7bf2
thomas.kellough <thomas.r.kellough@gmail.com>
2020-12-13 12:07:29 -0700

    practiced haptics

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -5,44 +5,102 @@
 //  Created by Thomas Kellough on 12/13/20.
 //
 
+import CoreHaptics
 import SwiftUI
 
 struct ContentView: View {
-    // how far the circle has been dragged
-    @State private var offset = CGSize.zero
-
-    // whether it is currently being dragged or not
-    @State private var isDragging = false
+    @State private var engine: CHHapticEngine?
+    @State private var showingAlert = false
 
     var body: some View {
-        // a drag gesture that updates offset and isDragging as it moves around
-        let dragGesture = DragGesture()
-            .onChanged { value in self.offset = value.translation }
-            .onEnded { _ in
-                withAnimation {
-                    self.offset = .zero
-                    self.isDragging = false
-                }
-            }
+        VStack {
+            Text("Hello, success")
+                .onTapGesture(perform: {
+                    simpleSuccess()
+                })
+                .padding()
+            Text("Hello, error")
+                .onTapGesture(perform: {
+                    simpleError()
+                })
+                .padding()
+            
+            Text("Hello, warning")
+                .onTapGesture(perform: {
+                    simpleWarning()
+                })
+                .padding()
+            
+            Text("Hello, complex")
+                .onAppear(perform: prepareHaptics)
+                .onTapGesture(perform: complexSuccess)
+                .padding()
+        }
+        .alert(isPresented: $showingAlert) {
+            Alert(title: Text("Error"), message: Text("Device does not support hapticsc"), dismissButton: .default(Text("Got it!")))
+        }
+    }
+    
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else {
+            self.showingAlert = true
+            return
+        }
+        
+        do {
+            self.engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was an error creating the engine: \(error.localizedDescription)")
+        }
+    }
+    
+    func complexSuccess() {
+        // make sure the device supports haptis
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        var events = [CHHapticEvent]()
+        
+//        // Create one intense, sharp tap
+//        let intensity  = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+//        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
+//        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
+//        events.append(event)
+        
+        // Create changing haptics
+        for i in stride(from: 0, to: 1, by: 0.1) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
+            events.append(event)
+        }
 
-        // a long press gesture that enables isDragging
-        let pressGesture = LongPressGesture()
-            .onEnded { value in
-                withAnimation {
-                    self.isDragging = true
-                }
-            }
-
-        // a combined gesture that forces the user to long press then drag
-        let combined = pressGesture.sequenced(before: dragGesture)
-
-        // a 64x64 circle that scales up when it's dragged, sets its offset to whatever we had back from the drag gesture, and uses our combined gesture
-        return Circle()
-            .fill(Color.red)
-            .frame(width: 64, height: 64)
-            .scaleEffect(isDragging ? 1.5 : 1)
-            .offset(offset)
-            .gesture(combined)
+        for i in stride(from: 0, to: 1, by: 0.1) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(1 - i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(1 - i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 1 + i)
+            events.append(event)
+        }
+        
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern: \(error.localizedDescription)")
+        }
+    }
+    
+    func simpleSuccess() {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(.success)
+    }
+    func simpleError() {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(.error)
+    }
+    func simpleWarning() {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(.warning)
     }
 }
 
