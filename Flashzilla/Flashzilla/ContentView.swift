a2d32b3a87ada37d4d8a164f542b9fd06d5c14c7
thomas.kellough <thomas.r.kellough@gmail.com>
2020-12-13 13:12:50 -0700

    practiced more accessibility needs

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -9,17 +9,64 @@ import CoreHaptics
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+    @Environment(\.accessibilityReduceMotion) var reduceMotion
+    @Environment(\.accessibilityReduceTransparency) var reduceTransparency
+    
+    @State private var scale: CGFloat = 1
 
     var body: some View {
-        Text("Hello, world")
-            .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { _ in
-                print("Moving to the background")
+        VStack {
+            HStack {
+                if differentiateWithoutColor {
+                    Image(systemName: "checkmark.circle")
+                }
+                
+                Text("Success")
             }
-        
-        Text("Hello, screenshot")
-            .onReceive(NotificationCenter.default.publisher(for: UIApplication.userDidTakeScreenshotNotification)) { _ in
-                print("User took a screenshot")
-            }
+            .padding()
+            .background(differentiateWithoutColor ? Color.black : Color.green)
+            .foregroundColor(Color.white)
+            .clipShape(Capsule())
+            
+            Text("Hello, world")
+                .scaleEffect(scale)
+                .onTapGesture {
+                    if self.reduceMotion {
+                        self.scale += 1.5
+                    } else {
+                        withAnimation {
+                            self.scale += 1.5
+                        }
+                    }
+                }
+                .padding()
+            
+            Text("Hello, world, with UIKit wrapper")
+                .scaleEffect(scale)
+                .onTapGesture {
+                    withOptionalAnimation {
+                        self.scale += 1.5
+                    }
+                }
+                .padding()
+            
+            Text("Hello, reduced transparency")
+                .padding()
+                .background(reduceTransparency ? Color.black : Color.black.opacity(0.5))
+                .foregroundColor(Color.white)
+                .clipShape(Capsule())
+            
+        }
+    }
+    
+    // UIKit wrapper function
+    func withOptionalAnimation<Result>(_ animation: Animation? = .default, _ body:() throws -> Result) rethrows -> Result {
+        if UIAccessibility.isReduceMotionEnabled {
+            return try body()
+        } else {
+            return try withAnimation(animation, body)
+        }
     }
     
 }
