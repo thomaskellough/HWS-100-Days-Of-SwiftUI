3c5d242c2d536f879d977965cdbbdf28cfdf560f
thomas.kellough <thomas.r.kellough@gmail.com>
2020-12-12 21:29:28 -0700

    added tab view for HotProspects

---
+++ HotProspects/HotProspects/Views/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by Thomas Kellough on 11/29/20.
+//
+
+import UserNotifications
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        TabView {
+            ProspectsView(filter: .none)
+                .tabItem {
+                    Image(systemName: "person.3")
+                    Text("Everyone")
+                }
+            ProspectsView(filter: .contacted)
+                .tabItem {
+                    Image(systemName: "checkmark.circle")
+                    Text("Contacted")
+                }
+            ProspectsView(filter: .uncontacted)
+                .tabItem {
+                    Image(systemName: "questionmark.diamond")
+                    Text("Uncontacted")
+                }
+            MeView()
+                .tabItem {
+                    Image(systemName: "person.crop.square")
+                    Text("Me")
+                }
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HotProspects/MeView.swift
@@ -0,0 +1,20 @@
+//
+//  MeView.swift
+//  HotProspects
+//
+//  Created by Thomas Kellough on 12/12/20.
+//
+
+import SwiftUI
+
+struct MeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MeView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeView()
+    }
+}
