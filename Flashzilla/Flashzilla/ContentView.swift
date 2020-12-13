5faf9ffc907e75caf870e4cf82fb57a70628ff42
thomas.kellough <thomas.r.kellough@gmail.com>
2020-12-13 12:32:33 -0700

    practiced allowHitTesting and contentShape

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -9,99 +9,36 @@ import CoreHaptics
 import SwiftUI
 
 struct ContentView: View {
-    @State private var engine: CHHapticEngine?
-    @State private var showingAlert = false
 
     var body: some View {
-        VStack {
-            Text("Hello, success")
-                .onTapGesture(perform: {
-                    simpleSuccess()
-                })
-                .padding()
-            Text("Hello, error")
-                .onTapGesture(perform: {
-                    simpleError()
-                })
-                .padding()
+        ZStack {
+            Rectangle()
+                .fill(Color.blue)
+                .frame(width: 300, height: 300)
+                .onTapGesture {
+                    print("Rectangle tapped")
+                }
+                .contentShape(Circle())
             
-            Text("Hello, warning")
-                .onTapGesture(perform: {
-                    simpleWarning()
-                })
-                .padding()
+            Circle()
+                .fill(Color.red)
+                .frame(width: 300, height: 300)
+                .onTapGesture {
+                    print("Cirlce tapped")
+                }
             
-            Text("Hello, complex")
-                .onAppear(perform: prepareHaptics)
-                .onTapGesture(perform: complexSuccess)
-                .padding()
-        }
-        .alert(isPresented: $showingAlert) {
-            Alert(title: Text("Error"), message: Text("Device does not support hapticsc"), dismissButton: .default(Text("Got it!")))
-        }
-    }
-    
-    func prepareHaptics() {
-        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else {
-            self.showingAlert = true
-            return
-        }
-        
-        do {
-            self.engine = try CHHapticEngine()
-            try engine?.start()
-        } catch {
-            print("There was an error creating the engine: \(error.localizedDescription)")
+            VStack {
+                Text("Hello")
+                Spacer().frame(height: 100)
+                Text("World")
+            }
+            .contentShape(Rectangle())
+            .onTapGesture {
+                print("Vstack tapped")
+            }
         }
     }
     
-    func complexSuccess() {
-        // make sure the device supports haptis
-        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
-        var events = [CHHapticEvent]()
-        
-//        // Create one intense, sharp tap
-//        let intensity  = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
-//        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
-//        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
-//        events.append(event)
-        
-        // Create changing haptics
-        for i in stride(from: 0, to: 1, by: 0.1) {
-            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
-            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
-            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
-            events.append(event)
-        }
-
-        for i in stride(from: 0, to: 1, by: 0.1) {
-            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(1 - i))
-            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(1 - i))
-            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 1 + i)
-            events.append(event)
-        }
-        
-        do {
-            let pattern = try CHHapticPattern(events: events, parameters: [])
-            let player = try engine?.makePlayer(with: pattern)
-            try player?.start(atTime: 0)
-        } catch {
-            print("Failed to play pattern: \(error.localizedDescription)")
-        }
-    }
-    
-    func simpleSuccess() {
-        let generator = UINotificationFeedbackGenerator()
-        generator.notificationOccurred(.success)
-    }
-    func simpleError() {
-        let generator = UINotificationFeedbackGenerator()
-        generator.notificationOccurred(.error)
-    }
-    func simpleWarning() {
-        let generator = UINotificationFeedbackGenerator()
-        generator.notificationOccurred(.warning)
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
